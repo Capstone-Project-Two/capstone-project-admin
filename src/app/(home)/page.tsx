94c11642fc18894fce0c4e823362a4a23efd0151
Sunchhay <sunchhay395@gmail.com>
2024-05-27 01:14:13 +0700

    add shadcn pagination component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/shadcn-ui/pagination"
 import { pageMetadata } from "@/utils/metadata-helpter"
 import { Metadata } from "next"
 
@@ -11,6 +12,22 @@ function Home({ }: Props) {
   return (
     <div>
       Capstone Admin
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="/users">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
     </div>
   )
 }
